fd27404d3cb3c88887583680463c097bbbd22204
Ronney Arias <ronneyarias@gmail.com>
2024-05-20 12:04:52 -0500

    complete lab

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,5 @@
 from vehicle import Vehicle
-
+          # v argument shows Car inherts from 'Vehicle' aka 'class Child(Parent)'
 class Car(Vehicle):
-    pass
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,13 @@
-class Vehicle:
-    pass
+class Vehicle: # parent or superclass
+                    # instances of Vehicle initialize with a wheel size and number. v       v
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    #  instance method 
+    def go(self):
+        return "vrrrrrrrooom!"
+    
+    #  instance method 
+    def fill_up_tank(self):
+        return "filling up!"
